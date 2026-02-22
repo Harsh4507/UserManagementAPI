a08e6989683411cca792eb25ad71ed9c1ac90878
Harsh Rajput <harshrajput4507@gmail.com>
2026-02-22 22:58:37 +0530

    Harsh : Understood Attribute Routing and LINQ.

--- UserManagement/Controllers/UserManagementController.cs
+++ UserManagement/Controllers/UserManagementController.cs
@@ -67,5 +67,41 @@ namespace UserManagement.Controllers
                 token = new JwtSecurityTokenHandler().WriteToken(token)
             });
         }
+
+        [HttpPost("LinqPractice")]
+        public ActionResult<string> LinqPractice([FromBody] User request)
+        {
+            var list = new List<User>()
+            {
+                new User { Id = 1, Name = "John Doe", Age = 30, Salary = 50000, Gender = "male" },
+                new User { Id = 2, Name = "Jane Smith", Age = 28, Salary = 60000, Gender = "memale" },
+                new User { Id = 3, Name = "Mike Johnson", Age = 35, Salary = 70000, Gender = "male" },
+                new User { Id = 4, Name = "Harsh", Age = 23, Salary = 35000, Gender = "male" },
+                new User { Id = 5, Name = "Rhino", Age = 23, Salary = 35000, Gender = "female" },
+                new User { Id = 6, Name = "Thakur", Age = 23, Salary = 47000, Gender = "female" },
+            };
+
+            var secondHighest = list.OrderByDescending(x => x.Salary).Skip(1).FirstOrDefault()!.Name;
+            var list2 = list.Where(x => x.Age > 23).ToList();
+            return "Login successful";
+        }
+
+        [HttpGet("GetUsers")]
+        public ActionResult<string> GetUsers()
+        {
+            return "Login successful";
+        }
+
+        [HttpGet("GetUsers/{id}/{name}")]
+        public ActionResult<string> GetUsers([FromRoute] int? id, [FromRoute] string? name)
+        {
+            return "Login successful";
+        }
+
+        [HttpGet("GetProduct")]
+        public ActionResult<string> GetProduct([FromQuery] string? name, [FromQuery] string? id)
+        {
+            return "Login successful";
+        }
     }
 }
